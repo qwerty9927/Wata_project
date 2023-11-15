d77ed1071ec49ca7cc6bc198242899e5808438e6
TobiNguyen <nat21102002@gmail.com>
2023-11-15 11:40:45 +0700

    [tuan] chore: config for validates and constance

---
+++ src/validates/index.js
@@ -0,0 +1,7 @@
+const productValidate = require("./product.validate");
+const productPriceValidate = require("./productPrice.validate");
+
+module.exports = {
+    productValidate,
+    productPriceValidate
+}
