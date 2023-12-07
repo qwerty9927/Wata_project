913e4685faf6bfdd1181cc5a5073136ca6848f1f
TobiNguyen <nat21102002@gmail.com>
2023-12-07 22:53:16 +0700

    [tuan] chore: handle error upload image

--- src/middlewares/handleMulterError.js
+++ src/middlewares/handleMulterError.js
@@ -3,14 +3,20 @@ const multer = require("multer");
 
 const handleMulterError = (err, req, res, next) => {
     if (err instanceof multer.MulterError) {
+        // console.log({ err });
         if (err.code === 'LIMIT_FILE_SIZE') {
-            // File size too large error
-            err = new ErrorResponse('File size too large, limit 3mb', 413);
+            // File size too large errors
+            return next(new ErrorResponse('File size too large, limit 3mb', 413));
+        }
+
+        if (err.code === 'FILE_NOT_ALLOWED') {
+            // File not allowed
+            return next(new ErrorResponse('File types must be png/jpg/gif/jpeg', 415));
         }
 
         if (err.code === 'LIMIT_UNEXPECTED_FILE') {
             // Limit unexpected file error
-            err = new ErrorResponse('Limit unexpected file', 422);
+            return next(new ErrorResponse('Limit unexpected file', 422));
         }
     }
     next(err);
