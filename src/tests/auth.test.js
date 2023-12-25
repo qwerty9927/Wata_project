902770f32fcebe3e7fb4adf481d74815ff8caf8e
qwerty9927 <tanvo9927@gmail.com>
2023-12-25 13:35:45 +0700

    [tan] test(auth): rewrite test case and cover logout case

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -1,158 +1,137 @@
-require("dotenv").config({path: require("path").join(__dirname, "../../.env")})
+require("dotenv").config({ path: require("path").join(__dirname, "../../.env.test.local") })
 const request = require("supertest")
 const app = require("../app")
-const AuthService = require("../services/auth.service")
 
-describe("Login api testing", () => {
+const login = async () => {
   const dataset = {
-    user_name: "abcdc",
-    user_password: "8e1823e4",
+    user_name: "admin",
+    user_password: "888888888",
   }
-  test("[Login] Test case 1: Empty any field", async () => {
-    const response = await request(app).post("/api/v1/auth/login")
+  const response = await request(app).post("/api/v1/auth/login").send(dataset)
+  return response.body.metadata.token
+}
+const token = (async () => await login())()
+
+describe("[Auth] Login api testing", () => {
+  // Standard data
+  // {
+  //   user_name: "admin",
+  //   user_password: "888888888",
+  // }
+  test("Test case 1: Validate failed", async () => {
+    const dataset = {
+      user_name: "",
+      user_password: "",
+    }
+    const response = await request(app).post("/api/v1/auth/login").send(dataset)
     expect(response.statusCode).toBe(422)
     expect(response.body.errors).toBeDefined()
   })
 
-  test("[Login] Test case 2: Username not found", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    // User not exist
-    Object.prototype.findOne = jest.fn(() => null)
+  test("Test case 2: Username not found", async () => {
+    const dataset = {
+      user_name: "fakeUsername",
+      user_password: "888888888",
+    }
     const response = await request(app).post("/api/v1/auth/login").send(dataset)
     expect(response.statusCode).toBe(401)
   })
 
-  test("[Login] Test case 3: Password not match", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    // User not exist
-    Object.prototype.findOne = jest.fn(() => ({
-      user_id: 11,
-      user_name: 'abcdc',
-      user_password: '$2b$10$SucLHtmBZkO98a47IQZvI.kHrS8nBYL1ofuzqAkZ1Vv3mP9o8fWEm',
-      full_name: 'MinhTan',
-      user_address: 'a/b/c',
-      user_phone: 778070683,
-      user_email: 'tanvo9927@gmail.com',
-      role_id: 2,
-      store_id: null,
-      user_role_relation: { role_id: 2, role_name: 'Customer' }
-    }))
-    const response = await request(app).post("/api/v1/auth/login").send({
-      user_name: dataset.user_name,
-      user_password: dataset.user_password + "1234"
-    })
+  test("Test case 3: Password not match", async () => {
+    const dataset = {
+      user_name: "admin",
+      user_password: "fakePassword",
+    }
+    const response = await request(app).post("/api/v1/auth/login").send(dataset)
     expect(response.statusCode).toBe(401)
   })
 
-  test("[Login] Test case 4: Login success", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    // Found user
-    Object.prototype.findOne = jest.fn(() => ({
-      user_id: 11,
-      user_name: 'abcdc',
-      user_password: '$2b$10$SucLHtmBZkO98a47IQZvI.kHrS8nBYL1ofuzqAkZ1Vv3mP9o8fWEm',
-      full_name: 'MinhTan',
-      user_address: 'a/b/c',
-      user_phone: 778070683,
-      user_email: 'tanvo9927@gmail.com',
-      role_id: 2,
-      store_id: null,
-      user_role_relation: { role_id: 2, role_name: 'Customer' }
-    }))
-    const response = await request(app).post("/api/v1/auth/login").send({
-      user_name: dataset.user_name,
-      user_password: dataset.user_password,
-    })
+  test("Test case 4: Login success", async () => {
+    const dataset = {
+      user_name: "admin",
+      user_password: "888888888",
+    }
+    const response = await request(app).post("/api/v1/auth/login").send(dataset)
     expect(response.statusCode).toBe(200)
-    expect(response.headers["set-cookie"].toString()).toMatch(
-      /token=([a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_.+/=]+);/
-    )
+    expect(response.body.metadata.token).toMatch(/[a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_=]+\.[a-zA-Z0-9-_.+/=]+/)
   })
 })
 
-describe("Register api testing", () => {
-  const dataset = {
-    user_name: "abcdc",
-    user_password: "8e1823e4",
-    full_name: "MinhTan",
-    user_address: "a/b/c",
-    user_phone: "0778070683",
-    user_email: "tanvo9927@gmail.com"
-  }
-  test("[Register] Test case 1: Empty any field", async () => {
+describe("[Auth] Register api testing", () => {
+  // Standard data
+  // {
+  //   user_name:"Staff1",
+  //   user_password:"888888888",
+  //   full_name:"TestUser",
+  //   user_phone:"0342800652",
+  //   user_email:"staff1@gmail.com"
+  // }
+  test("Test case 1: Validate failed", async () => {
     const response = await request(app).post("/api/v1/auth/register")
     expect(response.statusCode).toBe(422)
     expect(response.body.errors).toBeDefined()
   })
 
-  test("[Register] Test case 2: Conflict username or email", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    Object.prototype.findOneBy = jest.fn(() => ({
-      user_id: 11,
-      user_name: 'abcdc',
-      user_password: '$2b$10$SucLHtmBZkO98a47IQZvI.kHrS8nBYL1ofuzqAkZ1Vv3mP9o8fWEm',
-      full_name: 'MinhTan',
-      user_address: 'a/b/c',
-      user_phone: 778070683,
-      user_email: 'tanvo9927@gmail.com',
-      role_id: 2,
-      store_id: null
-    }))
+  test("Test case 2: Username or email was existed", async () => {
+    const dataset = {
+      user_name: "admin",
+      user_password: "888888888",
+      full_name: "MinhTan",
+      user_address: "a/b/c",
+      user_phone: "0778070683",
+      user_email: "tanvo9927@gmail.com",
+    }
     const response = await request(app).post("/api/v1/auth/register").send(dataset)
     expect(response.statusCode).toBe(409)
   })
 
-  test("[Register] Test case 3: Register success", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    Object.prototype.findOneBy = jest.fn(() => null)
-    Object.prototype.insert = jest.fn(() => true)
-    const response = await request(app).post("/api/v1/auth/register").send({
-      ...dataset,
-      user_name: dataset.user_name + "1234"
-    })
+  test("Test case 3: Register success", async () => {
+    const dataset = {
+      user_name: "Staff1",
+      user_password: "888888888",
+      full_name: "TestUser",
+      user_phone: "0342800652",
+      user_email: "staff1@gmail.com",
+    }
+    const response = await request(app).post("/api/v1/auth/register").send(dataset)
     expect(response.statusCode).toBe(201)
-    expect(response.body.metadata).toEqual({
-      full_name: "MinhTan",
-      user_address: "a/b/c",
-      user_phone: "0778070683",
-    })
+    expect(response.body.metadata).toBeDefined()
   })
 })
 
-describe("Reset password api testing", () => {
-  const dataset = {
-    user_email: "tanvo9927@gmail.com"
-  }
-  test("[Reset password] Test case 1: Empty any field", async () => {
+describe("[Auth] Reset password api testing", () => {
+  // Standard data
+  // {
+  //   user_email: "tanvo9927@gmail.com",
+  // }
+  test("Test case 1: Validate failed", async () => {
     const response = await request(app).post("/api/v1/auth/forgot-password")
     expect(response.statusCode).toBe(422)
     expect(response.body.errors).toBeDefined()
   })
 
-  test("[Reset password] Test case 2: Email is not signed", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    Object.prototype.findOneBy = jest.fn(() => null)
-    const response = await request(app).post("/api/v1/auth/forgot-password").send({
-      user_email: "abc@gmail.com"
-    })
+  test("Test case 2: Email is not signed", async () => {
+    const dataset = {
+      user_email: "fakeeamil@gmail.com",
+    }
+    const response = await request(app).post("/api/v1/auth/forgot-password").send(dataset)
     expect(response.statusCode).toBe(400)
   })
 
-  test("[Reset password] Test case 3: Reset password success", async () => {
-    AuthService.getUserRepository = jest.fn(() => ({}))
-    Object.prototype.findOneBy = jest.fn(() => ({
-      user_id: 11,
-      user_name: 'abcdc',
-      user_password: '$2b$10$SucLHtmBZkO98a47IQZvI.kHrS8nBYL1ofuzqAkZ1Vv3mP9o8fWEm',
-      full_name: 'MinhTan',
-      user_address: 'a/b/c',
-      user_phone: 778070683,
-      user_email: 'tanvo9927@gmail.com',
-      role_id: 2,
-      store_id: null
-    }))
-    Object.prototype.update = jest.fn(() => true)
+  test("Test case 3: Reset password success", async () => {
+    const dataset = {
+      user_email: "tanvo9927@gmail.com",
+    }
     const response = await request(app).post("/api/v1/auth/forgot-password").send(dataset)
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+describe("[Auth] Logout api testing", () => {
+  test("Test case 1: Logout success", async () => {
+    const response = await request(app)
+      .post("/api/v1/auth/logout")
+      .set({ Authorization: `Bearer ${await token}` })
     expect(response.statusCode).toBe(200)
   })
 })
